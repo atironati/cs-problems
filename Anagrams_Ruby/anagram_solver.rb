fea55aa8b54ba3b87c5f2bb51ec30042584c53ff
Alex Tironati <atironati@gmail.com>
2013-02-09 14:56:35 -0500

    properly format ruby, add time measurement

--- Anagrams_Ruby/anagram_solver.rb
+++ Anagrams_Ruby/anagram_solver.rb
@@ -1,25 +1,29 @@
 #!/usr/bin/env ruby
 
-wordsHash = {}
+words_hash = {}
 count = 0
 
 file = File.open(ARGV[0], "r")
 
-def sortLetters(word)
+start_time = Time.now
+
+def sort_letters(word)
   word.chars.sort{ |a, b| a.casecmp(b) }.join
 end
 
 while (word = file.gets)
-  letters = sortLetters(word)
-  if wordsHash.has_key?(letters)
-    count += wordsHash[letters] ? 1 : 2
-    wordsHash[letters] = true
+  letters = sort_letters(word)
+  if words_hash.has_key?(letters)
+    count += words_hash[letters] ? 1 : 2
+    words_hash[letters] = true
   else
-    wordsHash[letters] = false
+    words_hash[letters] = false
   end
 end
 
+end_time = Time.now
+puts "time: " + (end_time - start_time).to_s
 puts "count: " + count.to_s
 
-file.close
 
+file.close
