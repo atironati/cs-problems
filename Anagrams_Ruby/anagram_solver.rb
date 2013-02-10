2734773e61e9972b9df834a8cd574a5a1fc10926
Alex Tironati <atironati@gmail.com>
2013-02-09 21:48:08 -0500

    add verbose mode, update README

--- Anagrams_Ruby/anagram_solver.rb
+++ Anagrams_Ruby/anagram_solver.rb
@@ -1,29 +1,55 @@
 #!/usr/bin/env ruby
 
 words_hash = {}
+file = nil
+verbose_mode = false
 count = 0
 
-file = File.open(ARGV[0], "r")
+# parse arguments
+ARGV.each do |arg|
+  if arg == "-v" || arg == "--verbose"
+    verbose_mode = true
+  else
+    file = File.open(arg, "r")
+  end
+end
 
+# start timing
 start_time = Time.now
 
+# sort the characters in a word alphabetically, ignoring case
+# Ex. sort_letters( "tomato" ) -> "amoott"
 def sort_letters(word)
   word.chars.sort{ |a, b| a.casecmp(b) }.join
 end
 
+# loop through each word in the file
 while (word = file.gets)
   letters = sort_letters(word)
+
   if words_hash.has_key?(letters)
-    count += words_hash[letters] ? 1 : 2
-    words_hash[letters] = true
+    count += words_hash[letters][0] ? 1 : 2
+    words_hash[letters] = [true, words_hash[letters][1] << word.strip]
   else
-    words_hash[letters] = false
+    words_hash[letters] = [false, [word.strip]]
+  end
+end
+file.close
+
+# stop timing
+end_time = Time.now
+
+# loop through the words_hash and print groups of elements
+if verbose_mode
+  words_hash.each do |key, value|
+    if value[0]
+      value[1].each_with_index do |word, index|
+        last_word = index == value[1].length - 1
+        print last_word ? "#{word}\n" :  "#{word},"
+      end
+    end
   end
 end
 
-end_time = Time.now
-puts "time: " + (end_time - start_time).to_s
+puts "time: #{end_time - start_time}s"
 puts "count: " + count.to_s
-
-
-file.close
--- README.md
+++ README.md
@@ -3,6 +3,11 @@ cs-problems
 
 A repo for solutions to problems, programming exercises, or ideas related to computer science
 
+Anagram Solver
+--------------
 change permissions to executable: chmod +x anagram_solver.rb
 
-run: ./anagram_solver.rb
+word count: wc -l /usr/share/dict/words
+
+run: ./anagram_solver.rb example_words_file.txt [-v, --verbose]
+   - -v or --verbose will print out each grouping of anagrammed words
