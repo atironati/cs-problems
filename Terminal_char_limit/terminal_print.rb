1b2e0e0029e3cca3c48cbe977b18e4d0cce9e1ce
Alex Tironati <atironati@gmail.com>
2013-02-28 16:47:18 +1100

    get terminal print to actually work

--- Terminal_char_limit/terminal_print.rb
+++ Terminal_char_limit/terminal_print.rb
@@ -1,22 +1,25 @@
 #!/usr/bin/env ruby
 
-limit = ARGV[0]
+limit = ARGV[0].to_i
 
-def printInput (input)
+def printInput (limit, input)
+  delimiters = [" ", ",", ";"]
+
   input.each_line do |line|
-    if line.length >= limit
+    if line.length <= limit
       STDOUT.write line
     else
       index = line.length
       (line.length - 1).downto(0) do |i|
         index = i
-        break if line[i] = " "
+        break if delimiters.include? line[i]
       end
-      STDOUT.write line[0..index]
-      printInput line[index..-1]
+
+      STDOUT.write line[0..index] + "\n"
+      printInput limit, line[index..-1]
     end
   end
 end
 
 input = STDIN.read
-printInput input
+printInput limit, input
